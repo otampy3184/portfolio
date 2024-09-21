94f403aeae88f7e48f8d4c44c1404cd351e041f9
django_freeman <o.tampodo.t.18@gmail.com>
2024-09-22 00:54:19 +0900

    .mdx形式の確認

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,25 +1,32 @@
 "use client"
 import Link from "next/link";
 import { Navigation } from "../components/nav";
+import Image from "next/image";  // 追加
 
 export default function About() {
     return (
         <div className="w-screen h-screen bg-gradient-to-tr to-blue-500 from-green-500">
             <Navigation />
-			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-				<div className="max-w-2xl mx-auto lg:mx-0">
-					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-						About me
-					</h2>
-					<p className="mt-4 text-zinc-200 text-xl">
-					Hiroshi Takagi alias keccap256
-					</p>
-				</div>
-				<div className="w-full h-px bg-zinc-400" />
-				<img src="memes/somethingOf.jpg" alt="My Image" className="mt-8 mx-auto rounded-lg" />
-                {/* <p className="mt-4 text-zinc-100 text-base">
-					Wait, you guys are actually engineering?
-				</p> */}
+            <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+                <div className="max-w-2xl mx-auto lg:mx-0">
+                    <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+                        About me
+                    </h2>
+                    <p className="mt-4 text-zinc-200 text-xl">
+                        Hiroshi Takagi alias keccap256
+                    </p>
+                </div>
+                <div className="w-full h-px bg-zinc-400" />
+                <p className="mt-4 text-zinc-100 text-base">
+                    You know, I'm something of an engineer myself.
+                </p>
+                <Image 
+                    src="/memes/somethingOf.jpg"  // public フォルダ内のパスは / から始まる
+                    alt="My Image" 
+                    width={500} 
+                    height={500} 
+                    className="mt-8 mx-auto rounded-lg"
+                />
             </div>
         </div>
     )
